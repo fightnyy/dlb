a3c41d0e998a442d75510b01beeb320e277f86e1
fightnyy <55227984+fightnyy@users.noreply.github.com>
2021-03-21 19:39:10 +0900

    Update paws_data.py

--- gym/models/bart/paws_data.py
+++ gym/models/bart/paws_data.py
@@ -18,6 +18,7 @@ class PAWS_X(torch.utils.data.Dataset):
     src_lang : language of the input, example : English :en_XX, chinese:zh_CN, japanese:ja_XX, korean : ko_KR,
     tgt_lang : language of the output, example : English :en_XX, chinese:zh_CN, japanese:ja_XX, korean : ko_KR,
     """
+
     def __init__(self, infile: str, src_lang: str, tgt_lang: str,
                  max_len: int):
         self.tokenizer = MBart50TokenizerFast.from_pretrained(
@@ -40,8 +41,7 @@ class PAWS_X(torch.utils.data.Dataset):
                                     return_tensors="pt").input_ids
 
         input_ids = model_inputs['input_ids'][0].tolist()
-        if len(input_ids) > 128:
-            pdb.set_trace
+      
         pad_len = len(input_ids)
         pad = [self.tokenizer.pad_token_id] * (self.max_len - pad_len)
         input_ids.extend(pad)
@@ -50,6 +50,7 @@ class PAWS_X(torch.utils.data.Dataset):
         attn_pad = [0] * (self.max_len - pad_len)
         attn.extend(attn_pad)
         label = labels[0].tolist()
+
         labels_pad = [self.tokenizer.pad_token_id
                       ] * (self.max_len - len(label))
         label.extend(labels_pad)
@@ -68,6 +69,26 @@ class PAWS_X(torch.utils.data.Dataset):
         self.data.dropna()
         is_paraphrase = self.data['label'] == 1.0
         self.data = self.data[is_paraphrase]
+        length = len(self.data)
+        delete_list = []
+
+        
+        for i in range(length):
+
+          inputs = self.tokenizer(self.data.iloc[i]['sentence1'],return_tensors="pt")
+          inputs = inputs['input_ids'][0].tolist()
+
+          with self.tokenizer.as_target_tokenizer():
+            labels = self.tokenizer(self.data.iloc[i]['sentence2'], return_tensors="pt").input_ids
+          label = labels[0].tolist()
+          if len(inputs) > self.max_len :
+            delete_list.append(i)
+          if len(label) > self.max_len:
+            delete_list.append(i)
+        for idx, value in enumerate(delete_list):
+          index_name=self.data[self.data['id'] == self.data.iloc[value-idx].id].index
+          self.data.drop(index_name, inplace=True)
+
 
     # def make_padding(self, model_inputs:Dict,labels:torch.Tensor):
     #
